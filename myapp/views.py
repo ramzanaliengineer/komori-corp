a99cc45842651957706e2818f758cf3fc54aa000
Hafiz Ramzan Ali <your-email@example.com>
2025-12-02 21:36:04 +0500

    Codebase setup

---
+++ myapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from myapp.models import Contact
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    context = {
+        'variable1' : 'This is a sent',
+        'variable2' : 'This is a sent'
+    }
+    return render(request,'index.html',context)
+    # return HttpResponse('This is a black body')
+
+def about(request):
+    return render(request,'about.html')
+
+    # return HttpResponse('This is a about page')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, "Your message has been sent!")
+
+    return render(request,'contact.html')
+
+    # return HttpResponse('This is a contact page')
+
+def services(request):
+    return render(request,'services.html')
+
+    # return HttpResponse('This is a services page')
